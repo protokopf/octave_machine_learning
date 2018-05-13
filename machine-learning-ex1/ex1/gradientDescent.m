1bfb95778ce1a025888944e19808545d70acbc59
Konstantin Kovalenko <protokopf@gmail.com>
2018-05-13 20:13:56 +0300

    - Resolved nessesary issues

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,17 +16,18 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    predictions = X * theta;
+    difference = predictions - y; 
+    transposeX = X';
+    slopes = transposeX * difference;
 
-
-
-
-
-
+    theta = theta - (alpha/m) * slopes;
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    %plotData(1:iter, J_history);
 
 end
 
