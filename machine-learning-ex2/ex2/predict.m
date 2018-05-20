7e675533c3030c12946a2c8d404aed0c1e1121e0
Konstantin Kovalenko <protokopf@gmail.com>
2018-05-20 11:56:52 +0300

    - Implemented predict function.

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -14,14 +14,9 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
-
+p = sigmoid(X * theta);
 
 % =========================================================================
-
+p = floor(p + 0.5);
 
 end
