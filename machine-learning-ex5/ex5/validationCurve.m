6a3cca81d3685ae993087f19a5614de780b50486
Konstantin Kovalenko <protokopf@gmail.com>
2018-06-10 19:10:28 +0300

    - Implemented validation curve for lambda value.

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -11,6 +11,7 @@ function [lambda_vec, error_train, error_val] = ...
 
 % Selected values of lambda (you should not change this)
 lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+lambda_vec_size = size(lambda_vec);
 
 % You need to return these variables correctly.
 error_train = zeros(length(lambda_vec), 1);
@@ -38,16 +39,12 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
-
-
-
-
-
-
-
-
-
-
+for i = 1:lambda_vec_size
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X,y,lambda);
+  error_train(i) = linearRegCostFunction(X,y,theta, 0);
+  error_val(i) = linearRegCostFunction(Xval,yval,theta, 0);
+endfor
 % =========================================================================
 
 end
